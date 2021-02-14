7cd7db29e83c321794bae99811283d20df0303e4
sinemeren <sinem.eren.me@gmail.com>
2021-02-14 22:04:02 +0300

    new endpoint to test the html template

--- var/www/HelloWorldApp/app/views.py
+++ var/www/HelloWorldApp/app/views.py
@@ -1,7 +1,16 @@
-from app import app
+from . import app
+from flask import render_template
 
 #decorator
 @app.route('/')
+def hello():
+    return "Hello Sinem!"
 
-def home():
-    return "hello world!"
+@app.route('/template')
+def template():
+    return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
